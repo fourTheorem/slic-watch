a8ca0a1c6dab79818d548ed8e3e4c2afdca15712
Guilherme Dalla Rosa <gui_dr@msn.com>
2020-10-15 14:51:58 +0100

    using ThreadPoolExecutor to create alarms

--- watch/alarms.py
+++ watch/alarms.py
@@ -2,6 +2,7 @@ import boto3
 import os
 
 from aws_lambda_powertools import Logger
+from concurrent import futures
 from lambdas import get_applicable_lambdas
 
 LAMBDA_ERRORS_ALARM_NAME = 'LambdaErrosAlarm'
@@ -12,7 +13,7 @@ cloudwatch_client = boto3.client('cloudwatch')
 
 def create_lambda_alarm(func_name):
     """ Create an alarm for lambda errors """
-    response = cloudwatch_client.put_metric_alarm(
+    return cloudwatch_client.put_metric_alarm(
         AlarmName=f'LambdaError_{func_name}',
         Period=60,
         EvaluationPeriods=1,
@@ -32,10 +33,14 @@ def create_lambda_alarm(func_name):
         ]
     )
 
-    LOG.info(response)
-
 def update_alarms():
-  lambda_functions = get_applicable_lambdas()
+    lambda_functions = get_applicable_lambdas()
 
-  for func_name in lambda_functions.keys():
-      create_lambda_alarm(func_name)
+    with futures.ThreadPoolExecutor(max_workers=10) as executor:
+        wait_for = [
+            executor.submit(create_lambda_alarm, func_name)
+            for func_name in lambda_functions.keys()
+        ]
+
+        for future in futures.as_completed(wait_for):
+            LOG.info(future.result())
