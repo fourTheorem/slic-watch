53101f30358392b0eb43d33741e8637d949763ff
Guilherme Dalla Rosa <gui_dr@msn.com>
2020-10-16 17:14:04 +0100

    addressing review comments

--- watch/alarms.py
+++ watch/alarms.py
@@ -46,3 +46,33 @@ def update_alarms(errors_threshold=1.0, errors_period=60):
 
         for future in futures.as_completed(wait_for):
             LOG.info(future.result())
+
+def get_existing_alarm(func_name):
+  alarm_name = f'LambdaError_{func_name}'
+
+  alarms = cloudwatch_client.describe_alarms(
+    AlarmNames=[
+      alarm_name
+    ]
+  )
+
+  existing_alarm = next(
+    (_ for _ in alarms['MetricAlarms'] if _['AlarmName'] == alarm_name),
+    None
+  )
+
+  return existing_alarm
+
+def delete_alarms(func_name):
+  existing_alarm = get_existing_alarm(func_name)
+
+  if existing_alarm:
+    response = cloudwatch_client.delete_alarms(
+      AlarmNames=[
+        existing_alarm['AlarmName']
+      ]
+    )
+
+    LOG.info(response)
+  else:
+    LOG.info(f'No alarms found for function {func_name}')
