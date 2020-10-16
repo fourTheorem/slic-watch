eb6efccc20226abdd358a34077287210fd094f01
Guilherme Dalla Rosa <gui_dr@msn.com>
2020-10-16 17:27:14 +0100

    Remove alarms when functions are deleted (#3)
    * lambda function to remove alarms when functions are deleted
    
    * addressing review comments

---
+++ watch/delete_handler.py
@@ -0,0 +1,21 @@
+"""
+Lambda handler module invoked during stack delete.
+Delete alarms for deleted resouces
+"""
+import boto3
+
+from alarms import delete_alarms
+from aws_lambda_powertools import Logger
+
+LOG = Logger()
+cw_client = boto3.client('cloudwatch')
+
+@LOG.inject_lambda_context
+def delete_resources(event, _):
+    """ Handle delete of CloudFormation stacks """
+    parameters = event.get('detail', {})['requestParameters']
+
+    if 'functionName' in parameters:
+        delete_alarms(func_name=parameters['functionName'])
+
+    return {}
