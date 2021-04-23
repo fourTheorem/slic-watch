fee2607eccfaee0704971ad85a6dc521a0226267
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2021-04-23 10:09:43 +0100

    Update serverless-test-project/apigw-handler.js
    Co-authored-by: Luciano Mammino <lucianomammino@gmail.com>

--- serverless-test-project/apigw-handler.js
+++ serverless-test-project/apigw-handler.js
@@ -4,6 +4,9 @@ module.exports = {
   handleGet,
 }
 
+/**
+ * A simple Lambda that can be used to test metrics and alarms for different status codes and response times
+ */
 async function handleGet(event) {
   const qs = event.queryStringParameters || {}
   const timeout = Number(qs.timeout) || 1000
