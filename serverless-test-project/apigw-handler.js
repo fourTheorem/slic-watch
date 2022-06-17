fb9fe47e6eba686a4033a3eb5abb0ba6d029ed45
direnakkocdemir <drnakkoc94@gmail.com>
2022-06-17 16:43:38 +0100

    New subscription added to the SNS topic - Doing integration test with real resources by adding https endpoint and creating lambda function to handle confirmSubscription for SNS topic

--- serverless-test-project/apigw-handler.js
+++ serverless-test-project/apigw-handler.js
@@ -1,12 +1,16 @@
 'use strict'
+const https = require('https')
+const MessageValidator = require('sns-validator')
 
 module.exports = {
-  handleGet
+  handleGet,
+  handleSubcription
 }
 
 /**
  * A simple Lambda that can be used to test metrics and alarms for different status codes and response times
  */
+
 async function handleGet (event) {
   const qs = event.queryStringParameters || {}
   const timeout = Number(qs.timeout) || 1000
@@ -17,4 +21,28 @@ async function handleGet (event) {
   })
 
   return { statusCode }
+}
+
+async function handleSubcription (event) {
+  console.log(event)
+  const validator = new MessageValidator()
+  return new Promise(function (resolve, reject) {
+    validator.validate(JSON.parse(event.body), (err, message) => {
+      if (err) {
+        console.error(err)
+        reject(err)
+      }
+      validator.encoding = 'utf8'
+      if (message.Type === 'SubscriptionConfirmation') {
+        https.get(message.SubscribeURL, (res) => {
+          resolve({ statusCode: res.statusCode })
+        }).on('error', (e) => {
+          reject(e)
+        }
+        )
+      } else {
+        resolve({ statusCode: 200 })
+      }
+    })
+  })
 }
