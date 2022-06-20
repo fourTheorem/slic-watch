4294b64028b176855e57a419f692929dc5e3c561
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2022-06-20 14:07:26 +0100

    Enable failed deliveries in SNS HTTPS subscription endpoint

--- serverless-test-project/apigw-handler.js
+++ serverless-test-project/apigw-handler.js
@@ -4,7 +4,7 @@ const MessageValidator = require('sns-validator')
 
 module.exports = {
   handleGet,
-  handleSubcription
+  handleSubscription
 }
 
 /**
@@ -23,14 +23,27 @@ async function handleGet (event) {
   return { statusCode }
 }
 
-async function handleSubcription (event) {
+/**
+ * This Lambda function handler allows us to provide an HTTP endpoint for SNS HTTP(S) subscriptions.
+ * It handles two event types:
+ *  - Message subscription confirmation flow, where SNS requires the HTTPS endpoint to validate the subscription.
+ *  - Message delivery
+ *
+ * In the case of message delivery, we can also simulate message delivery failures. If the message payload has `fail: true`,
+ * we will return a 500 status code.
+ *
+ * @param {object} event The Lambda event
+ * @returns The API Gateway Lambda Proxy response
+ */
+async function handleSubscription (event) {
   console.log(event)
   const validator = new MessageValidator()
-  return new Promise(function (resolve, reject) {
-    validator.validate(JSON.parse(event.body), (err, message) => {
+  const promise = new Promise(function (resolve, reject) {
+    const eventBody = JSON.parse(event.body)
+    validator.validate(eventBody, (err, message) => {
       if (err) {
         console.error(err)
-        reject(err)
+        return reject(err)
       }
       validator.encoding = 'utf8'
       if (message.Type === 'SubscriptionConfirmation') {
@@ -38,11 +51,18 @@ async function handleSubcription (event) {
           resolve({ statusCode: res.statusCode })
         }).on('error', (e) => {
           reject(e)
-        }
-        )
+        })
       } else {
+        const messageBody = JSON.parse(eventBody.Message)
+        if (messageBody.fail === true) {
+          return resolve({ statusCode: 500 })
+        }
         resolve({ statusCode: 200 })
       }
     })
   })
+
+  const result = await promise
+  console.log('Result', result)
+  return result
 }
