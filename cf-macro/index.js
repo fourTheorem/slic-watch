9e99f2e5b76e0520ee4539cdd72647ecdb4a4327
Stella <stella@Rias-MacBook-Air.local>
2022-08-04 09:56:41 +0100

    Load slicwatchconfig from the client template except SNS topic

--- cf-macro/index.js
+++ cf-macro/index.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const Ajv = require('ajv')
+
 const alarms = require('slic-watch-core/alarms')
 const dashboard = require('slic-watch-core/dashboard')
 const CloudFormationTemplate = require('slic-watch-core/cf-template')
 const defaultConfig = require('slic-watch-core/default-config')
+const {  functionConfigSchema, slicWatchSchema } = require('slic-watch-core/config-schema')
 
 exports.handler =  async function(event, context) {
   console.info("Received event", {event})
@@ -17,38 +20,58 @@ function processFragment( event ){
    let outputFragment = event.fragment; 
     try {
 
-      const alarmActions = []
-      if (process.env.SNSTopic) {
-        alarmActions.push(process.env.SNSTopic)
+      const slicWatchConfig = (outputFragment.Metadata || {}).slicWatch || {}
+     
+      const ajv = new Ajv({
+        unicodeRegExp: false
+      })
+      const slicWatchValidate = ajv.compile(slicWatchSchema)
+      const slicWatchValid = slicWatchValidate(slicWatchConfig)
+      if (!slicWatchValid) {
+        throw new Error('SLIC Watch configuration is invalid: ' + ajv.errorsText(slicWatchValidate.errors))
       }
-      const context = {
-        region: event.region,
-        stackName: (event.templateParameterValues.stack)?event.templateParameterValues.stack : "",
-        alarmActions
-      }
+      
+      if (slicWatchConfig.enabled === true) {
 
-      const cfTemplate = CloudFormationTemplate(
-        outputFragment
-      )
-      const serverless = {cli:console};
-      const functionAlarmConfigs = {}
-      const functionDashboardConfigs = {}
+        const alarmActions = []
 
-      const lambdaResources = cfTemplate.getResourcesByType(
-        'AWS::Lambda::Function'
-      );
+        if (slicWatchConfig.topicArn) {
+          alarmActions.push(slicWatchConfig.topicArn)
+        }
+        else if (process.env.SNSTopic){
+          alarmActions.push(process.env.SNSTopic)
+        }
+        console.log('alarmActions', alarmActions)
 
-      for (const [funcResourceName, funcResource] of Object.entries(lambdaResources)) {
-        const funcConfig = funcResource.Metadata.slicWatch || {}
-        functionAlarmConfigs[funcResource.Properties.FunctionName] =  funcConfig.alarms || {}
-        functionDashboardConfigs[funcResource.Properties.FunctionName] =  funcConfig.dashboard || {}
-      }
+        const context = {
+          region: event.region,
+          stackName: (event.templateParameterValues.stack)?event.templateParameterValues.stack : "",
+          alarmActions
+        }
 
-      const alarmService = alarms(serverless, defaultConfig.alarms, functionAlarmConfigs, context)
-      alarmService.addAlarms(cfTemplate)
-      const dashboardService = dashboard(serverless, defaultConfig.dashboard, functionDashboardConfigs, context)
-      dashboardService.addDashboard(cfTemplate)
-      outputFragment = cfTemplate.getSourceObject();
+        const cfTemplate = CloudFormationTemplate(
+          outputFragment
+        )
+        const serverless = {cli:console};
+        const functionAlarmConfigs = {}
+        const functionDashboardConfigs = {}
+
+        const lambdaResources = cfTemplate.getResourcesByType(
+          'AWS::Lambda::Function'
+        );
+
+        for (const [funcResourceName, funcResource] of Object.entries(lambdaResources)) {
+          const funcConfig = funcResource.Metadata.slicWatch || {}
+          functionAlarmConfigs[funcResource.Properties.FunctionName] =  funcConfig.alarms || {}
+          functionDashboardConfigs[funcResource.Properties.FunctionName] =  funcConfig.dashboard || {}
+        }
+
+        const alarmService = alarms(serverless, defaultConfig.alarms, functionAlarmConfigs, context)
+        alarmService.addAlarms(cfTemplate)
+        const dashboardService = dashboard(serverless, defaultConfig.dashboard, functionDashboardConfigs, context)
+        dashboardService.addDashboard(cfTemplate)
+        outputFragment = cfTemplate.getSourceObject();
+    }
      
     } catch (err) {
       console.error(err);
@@ -57,7 +80,7 @@ function processFragment( event ){
     
     return {
         "requestId": event.requestId,
-        "status": status,
+        "status": "fail",
         "fragment": outputFragment
     }
 }
