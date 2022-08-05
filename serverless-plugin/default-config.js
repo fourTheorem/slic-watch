9fded10371438a38ac7c9258aa45be4d6664f3a0
Stella <stella@Rias-MacBook-Air.local>
2022-08-05 18:43:27 +0100

    Serverless plugin modified to use core package

--- serverless-plugin/default-config.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const { join } = require('path')
-const { readFileSync } = require('fs')
-const YAML = require('yaml')
-
-const yamlConfigSource = readFileSync(join(__dirname, 'default-config.yaml'), 'utf-8')
-
-module.exports = YAML.parse(yamlConfigSource)
