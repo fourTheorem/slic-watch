ae979e6652060891f37498b921d6f06b82ca49f3
ssamuel <ssamuel@ssamuels-MacBook-Pro.local>
2022-08-26 19:10:38 +0100

    review fixes

--- cf-macro/tests/index.test.js
+++ cf-macro/tests/index.test.js
@@ -36,22 +36,25 @@ test('index', t => {
     const result = await lambda.handler(event, null)
     t.equal(result.status, 'success')
   })
+
   t.test('Macro skips SLIC Watch if top-level enabled==false', async t => {
     const testevent = _.cloneDeep(event)
     testevent.fragment.Metadata.slicWatch.enabled = false
     await lambda.handler(testevent, null)
     t.notOk(testState.alarmsCalled)
   })
+
   t.test('Macro adds dashboard and alarms', async t => {
     await lambda.handler(event, null)
     t.equal(testState.addDashboardCfTemplate.getSourceObject(), template)
     t.equal(testState.addAlarmsCfTemplate.getSourceObject(), template)
   })
+
   t.test('Plugin execution fails if an invalid SLIC Watch config is provided', async t => {
     const testevent = _.cloneDeep(event)
     testevent.fragment.Metadata.slicWatch.topicArrrrn = 'pirateTopic'
-    await lambda.handler(testevent, null)
-    t.throws(() => { throw new Error('SLIC Watch configuration is invalid: data must NOT have additional properties') }, {}, { skip: true })
+    const result = await lambda.handler(testevent, null)
+    t.equal(result.status, 'fail')
   })
 
   t.test('Plugin execution succeeds with no slicWatch config', async t => {
@@ -60,12 +63,14 @@ test('index', t => {
     await lambda.handler(testevent, null)
     t.ok(testState.alarmsCalled)
   })
+
   t.test('Plugin execution succeeds if no SNS Topic is provided', async t => {
     const testevent = _.cloneDeep(event)
     delete testevent.fragment.Metadata.slicWatch.topicArn
     await lambda.handler(testevent, null)
     t.ok(testState.alarmsCalled)
   })
+
   t.test('Plugin succeeds with no custom section', async t => {
     const testevent = _.cloneDeep(event)
     delete testevent.fragment.Metadata
