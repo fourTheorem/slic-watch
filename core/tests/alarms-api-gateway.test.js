43c9a2b9338d7ba887080c5da4fbdfdd0f3f8319
Eoin Shanaghy <eoin.shanaghy@fourtheorem.com>
2023-04-26 22:37:13 +0100

    fix(core): fix alarm logical IDs for API GWs with object names
    - Ensure correct logical IDs even when intrinsic functions used in API
      Gateway name

--- core/tests/alarms-api-gateway.test.js
+++ core/tests/alarms-api-gateway.test.js
@@ -36,112 +36,137 @@ test('API Gateway alarms are created', (t) => {
   const apiGwAlarmConfig = alarmConfig.ApiGateway
 
   const { createApiGatewayAlarms } = apiGatewayAlarms(apiGwAlarmConfig, testContext)
-  const cfTemplate = createTestCloudFormationTemplate()
-  createApiGatewayAlarms(cfTemplate)
 
-  const alarmResources = cfTemplate.getResourcesByType('AWS::CloudWatch::Alarm')
+  t.test('with full template', (t) => {
+    const cfTemplate = createTestCloudFormationTemplate()
+    createApiGatewayAlarms(cfTemplate)
 
-  const alarmsByType = {}
-  t.equal(Object.keys(alarmResources).length, 3)
-  for (const alarmResource of Object.values(alarmResources)) {
-    const al = alarmResource.Properties
-    assertCommonAlarmProperties(t, al)
-    const alarmType = alarmNameToType(al.AlarmName)
-    alarmsByType[alarmType] = alarmsByType[alarmType] || new Set()
-    alarmsByType[alarmType].add(al)
-  }
+    const alarmResources = cfTemplate.getResourcesByType('AWS::CloudWatch::Alarm')
 
-  t.same(Object.keys(alarmsByType).sort(), [
-    'APIGW_4XXError',
-    'APIGW_5XXError',
-    'APIGW_Latency'
-  ])
+    const alarmsByType = {}
+    t.equal(Object.keys(alarmResources).length, 3)
+    for (const alarmResource of Object.values(alarmResources)) {
+      const al = alarmResource.Properties
+      assertCommonAlarmProperties(t, al)
+      const alarmType = alarmNameToType(al.AlarmName)
+      alarmsByType[alarmType] = alarmsByType[alarmType] || new Set()
+      alarmsByType[alarmType].add(al)
+    }
 
-  t.equal(alarmsByType.APIGW_5XXError.size, 1)
-  for (const al of alarmsByType.APIGW_5XXError) {
-    t.equal(al.MetricName, '5XXError')
-    t.equal(al.Statistic, 'Average')
-    t.equal(al.Threshold, apiGwAlarmConfig['5XXError'].Threshold)
-    t.equal(al.EvaluationPeriods, 2)
-    t.equal(al.TreatMissingData, 'breaching')
-    t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
-    t.equal(al.Namespace, 'AWS/ApiGateway')
-    t.equal(al.Period, 120)
-    t.same(al.Dimensions, [
-      {
-        Name: 'ApiName',
-        Value: 'dev-serverless-test-project'
-      }
+    t.same(Object.keys(alarmsByType).sort(), [
+      'APIGW_4XXError',
+      'APIGW_5XXError',
+      'APIGW_Latency'
     ])
-  }
 
-  for (const al of alarmsByType.APIGW_4XXError) {
-    t.equal(al.MetricName, '4XXError')
-    t.equal(al.Statistic, 'Average')
-    t.equal(al.Threshold, apiGwAlarmConfig['4XXError'].Threshold)
-    t.equal(al.EvaluationPeriods, 2)
-    t.equal(al.TreatMissingData, 'breaching')
-    t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
-    t.equal(al.Namespace, 'AWS/ApiGateway')
-    t.equal(al.Period, 120)
-    t.same(al.Dimensions, [
+    t.equal(alarmsByType.APIGW_5XXError.size, 1)
+    for (const al of alarmsByType.APIGW_5XXError) {
+      t.equal(al.MetricName, '5XXError')
+      t.equal(al.Statistic, 'Average')
+      t.equal(al.Threshold, apiGwAlarmConfig['5XXError'].Threshold)
+      t.equal(al.EvaluationPeriods, 2)
+      t.equal(al.TreatMissingData, 'breaching')
+      t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
+      t.equal(al.Namespace, 'AWS/ApiGateway')
+      t.equal(al.Period, 120)
+      t.same(al.Dimensions, [
+        {
+          Name: 'ApiName',
+          Value: 'dev-serverless-test-project'
+        }
+      ])
+    }
+
+    for (const al of alarmsByType.APIGW_4XXError) {
+      t.equal(al.MetricName, '4XXError')
+      t.equal(al.Statistic, 'Average')
+      t.equal(al.Threshold, apiGwAlarmConfig['4XXError'].Threshold)
+      t.equal(al.EvaluationPeriods, 2)
+      t.equal(al.TreatMissingData, 'breaching')
+      t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
+      t.equal(al.Namespace, 'AWS/ApiGateway')
+      t.equal(al.Period, 120)
+      t.same(al.Dimensions, [
+        {
+          Name: 'ApiName',
+          Value: 'dev-serverless-test-project'
+        }
+      ])
+    }
+
+    for (const al of alarmsByType.APIGW_Latency) {
+      t.equal(al.MetricName, 'Latency')
+      t.equal(al.ExtendedStatistic, 'p99')
+      t.equal(al.Threshold, apiGwAlarmConfig.Latency.Threshold)
+      t.equal(al.EvaluationPeriods, 2)
+      t.equal(al.TreatMissingData, 'breaching')
+      t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
+      t.equal(al.Namespace, 'AWS/ApiGateway')
+      t.equal(al.Period, 120)
+      t.same(al.Dimensions, [
+        {
+          Name: 'ApiName',
+          Value: 'dev-serverless-test-project'
+        }
+      ])
+    }
+
+    t.end()
+  })
+
+  test('API Gateway alarms are not created when disabled globally', (t) => {
+    const alarmConfig = createTestConfig(
+      defaultConfig.alarms,
       {
-        Name: 'ApiName',
-        Value: 'dev-serverless-test-project'
-      }
-    ])
-  }
-
-  for (const al of alarmsByType.APIGW_Latency) {
-    t.equal(al.MetricName, 'Latency')
-    t.equal(al.ExtendedStatistic, 'p99')
-    t.equal(al.Threshold, apiGwAlarmConfig.Latency.Threshold)
-    t.equal(al.EvaluationPeriods, 2)
-    t.equal(al.TreatMissingData, 'breaching')
-    t.equal(al.ComparisonOperator, 'GreaterThanOrEqualToThreshold')
-    t.equal(al.Namespace, 'AWS/ApiGateway')
-    t.equal(al.Period, 120)
-    t.same(al.Dimensions, [
-      {
-        Name: 'ApiName',
-        Value: 'dev-serverless-test-project'
-      }
-    ])
-  }
-
-  t.end()
-})
-
-test('API Gateway alarms are not created when disabled globally', (t) => {
-  const alarmConfig = createTestConfig(
-    defaultConfig.alarms,
-    {
-      ApiGateway: {
-        enabled: false, // disabled globally
-        Period: 60,
-        '5XXError': {
-          Threshold: 0.0
-        },
-        '4XXError': {
-          Threshold: 0.05
-        },
-        Latency: {
-          Threshold: 5000
+        ApiGateway: {
+          enabled: false, // disabled globally
+          Period: 60,
+          '5XXError': {
+            Threshold: 0.0
+          },
+          '4XXError': {
+            Threshold: 0.05
+          },
+          Latency: {
+            Threshold: 5000
+          }
         }
       }
-    }
-  )
+    )
 
-  const apiGwAlarmConfig = alarmConfig.ApiGateway
+    const apiGwAlarmConfig = alarmConfig.ApiGateway
 
-  const { createApiGatewayAlarms } = apiGatewayAlarms(apiGwAlarmConfig, testContext)
+    const { createApiGatewayAlarms } = apiGatewayAlarms(apiGwAlarmConfig, testContext)
 
-  const cfTemplate = createTestCloudFormationTemplate()
-  createApiGatewayAlarms(cfTemplate)
+    const cfTemplate = createTestCloudFormationTemplate()
+    createApiGatewayAlarms(cfTemplate)
 
-  const alarmResources = cfTemplate.getResourcesByType('AWS::CloudWatch::Alarm')
+    const alarmResources = cfTemplate.getResourcesByType('AWS::CloudWatch::Alarm')
 
-  t.same({}, alarmResources)
+    t.same({}, alarmResources)
+    t.end()
+  })
+
+  t.test('alarm logical IDs are correct when an intrinsic function is used in the API Gateway name', (t) => {
+    const cfTemplate = createTestCloudFormationTemplate({
+      Resources: {
+        myApi: {
+          Type: 'AWS::ApiGateway::RestApi',
+          Properties: {
+            Name: { Ref: 'AWS::StackName' }
+          }
+        }
+      }
+    })
+    createApiGatewayAlarms(cfTemplate)
+    const alarmResources = cfTemplate.getResourcesByType('AWS::CloudWatch::Alarm')
+    t.same(Object.keys(alarmResources).sort(), [
+      'slicWatchApi4XXErrorAlarmMyApi',
+      'slicWatchApiAvailabilityAlarmMyApi',
+      'slicWatchApiLatencyAlarmMyApi'
+    ])
+    t.end()
+  })
   t.end()
 })
 
